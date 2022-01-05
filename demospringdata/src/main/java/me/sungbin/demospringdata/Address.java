73adba2308a6e3756c07d02c082865c488e832c2
SungbinYang <ysb05222@naver.com>
2022-01-06 00:02:37 +0900

    JPA 프로그래밍: Value 타입 맵핑

---
+++ demospringdata/src/main/java/me/sungbin/demospringdata/Address.java
@@ -0,0 +1,20 @@
+package me.sungbin.demospringdata;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@Setter
+@Embeddable
+public class Address {
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+}
