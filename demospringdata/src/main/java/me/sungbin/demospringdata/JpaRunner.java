e59e1640c764a93c4de080e744ec744e33fa99e2
SungbinYang <ysb05222@naver.com>
2022-01-14 22:19:08 +0900

    스프링 데이터 JPA 원리 추가

--- demospringdata/src/main/java/me/sungbin/demospringdata/JpaRunner.java
+++ demospringdata/src/main/java/me/sungbin/demospringdata/JpaRunner.java
@@ -1,6 +1,6 @@
 package me.sungbin.demospringdata;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,13 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Transactional
+@RequiredArgsConstructor
 public class JpaRunner implements ApplicationRunner {
 
-    @Autowired
-    PostRepository postRepository;
+    final PostRepository postRepository;
 
-    @Autowired
-    Sungbin sungbin;
+    final Sungbin sungbin;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
